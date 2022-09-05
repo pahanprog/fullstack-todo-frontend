39a884fbe4f295c02ea3f6db0f06a3743f355606
p.s.ledyaev <nninta05@gmail.com>
2022-09-05 20:06:05 +0300

    Setting up heroku

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import ContextProvider from "./contexts/ContextProvider";
 import "./index.css";
 import Router from "./Router";
 
-axios.defaults.baseURL = "http://localhost:4000/";
+axios.defaults.baseURL = "https://ps-todo-backend.herokuapp.com/";
 axios.defaults.withCredentials = true;
 axios.defaults.headers.common["Accept"] = "application/json";
 axios.defaults.headers.post["Content-Type"] = "application/json";
